71865c97f2cb9d13072dc320d191633b139f760e
Jeremy Tupas <jeremytupas@gmail.com>
2022-08-23 11:49:33 -0500

    Refactored timer and targets logic

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
+import { targets } from "./targets.js";
 import { timer } from "./timer.js";
 
-const target1Discovered = () => {
-  console.log("target 1 discovered");
-};
-
-const target2Discovered = () => {
-  console.log("target 2 discovered");
-};
-
-const target3Discovered = () => {
-  console.log("target 3 discovered");
-};
-
-const gameTimer = timer();
-gameTimer.start();
-setInterval(() => {
-  const timeInSeconds = Math.round(gameTimer.getTime() / 1000);
-  document.querySelector(".timer").textContent = timeInSeconds;
-}, 100);
+targets();
+timer();
--- timer.js
+++ timer.js
@@ -1,56 +1,71 @@
 const timer = () => {
-  let isRunning = false;
-  let startTime = 0;
-  let overallTime = 0;
+  const timerFactory = () => {
+    let isRunning = false;
+    let startTime = 0;
+    let overallTime = 0;
 
-  const _getTimeElapsedSinceLastStart = () => {
-    // console.log('run getTimeElapsed');
-    if (!startTime) {
-      return 0;
-    }
-    return Date.now() - startTime;
-  };
+    const _getTimeElapsedSinceLastStart = () => {
+      // console.log('run getTimeElapsed');
+      if (!startTime) {
+        return 0;
+      }
+      return Date.now() - startTime;
+    };
 
-  const start = () => {
-    // console.log('run start');
-    if (isRunning) {
-      return console.error("Time is already running");
-    }
-    isRunning = true;
-    startTime = Date.now();
-  };
-
-  const stop = () => {
-    // console.log('run stop');
-    if (!isRunning) {
-      return console.error("Timer is already stopped");
-    }
-    isRunning = false;
-    overallTime = overallTime + _getTimeElapsedSinceLastStart();
-  };
-
-  const reset = () => {
-    // console.log('run reset');
-    overallTime = 0;
-    if (isRunning) {
+    const start = () => {
+      // console.log('run start');
+      if (isRunning) {
+        return console.error("Time is already running");
+      }
+      isRunning = true;
       startTime = Date.now();
-      return;
-    }
-    startTime = 0;
+    };
+
+    const stop = () => {
+      // console.log('run stop');
+      if (!isRunning) {
+        return console.error("Timer is already stopped");
+      }
+      isRunning = false;
+      overallTime = overallTime + _getTimeElapsedSinceLastStart();
+    };
+
+    const reset = () => {
+      // console.log('run reset');
+      overallTime = 0;
+      if (isRunning) {
+        startTime = Date.now();
+        return;
+      }
+      startTime = 0;
+    };
+
+    const getTime = () => {
+      // console.log('run getTime');
+      if (!startTime) {
+        return 0;
+      }
+      if (isRunning) {
+        return overallTime + _getTimeElapsedSinceLastStart();
+      }
+      return overallTime;
+    };
+
+    return { start, stop, reset, getTime };
   };
 
-  const getTime = () => {
-    // console.log('run getTime');
-    if (!startTime) {
-      return 0;
-    }
-    if (isRunning) {
-      return overallTime + _getTimeElapsedSinceLastStart();
-    }
-    return overallTime;
+  const gameTimer = timerFactory();
+
+  const startTimer = () => {
+    gameTimer.start();
+    setInterval(() => {
+      const timeInSeconds = Math.round(gameTimer.getTime() / 1000);
+      document.querySelector(".timer").textContent = timeInSeconds;
+    }, 100);
   };
 
-  return { start, stop, reset, getTime };
+  const startButton = document.querySelector(".start-button");
+  startButton.addEventListener("click", startTimer);
 };
 
 export { timer };
